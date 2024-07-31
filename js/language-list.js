4b2602c06189210ec1e609cfb745eb84df5593d2
ivan41kd <grkorolevskiy@gmail.com>
2024-07-31 20:20:00 +0300

    add: missing margin, other fixes for footer, main

--- js/language-list.js
+++ js/language-list.js
@@ -1,5 +1,5 @@
 const languageWrapper = document.querySelectorAll('.header__language-wrapper');
-if (window.innerWidth <= 872) {
+if (window.innerWidth < 1410) {
  languageWrapper.forEach((language) => {
   const languageList = language.querySelector('.header__language-list');
   const languageItem = languageList.querySelectorAll('.header__language-item');
@@ -10,7 +10,13 @@ if (window.innerWidth <= 872) {
     languageItem.forEach((item) => {
      item.addEventListener('click', () => {
       const currentLanguage = language.querySelector('.header__language');
-      currentLanguage.textContent = item.textContent;
+      const currentLanguage2 = language.querySelector('.header__item.language');
+      if (currentLanguage2) {
+       currentLanguage2.textContent = item.textContent;
+      }
+      if (currentLanguage) {
+       currentLanguage.textContent = item.textContent;
+      }
      });
     });
    } else languageList.classList.remove('active');
