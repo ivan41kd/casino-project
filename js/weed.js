4e59a3a3a1b64bb39c4580a2570e0ede177b24a0
ivan41kd <grkorolevskiy@gmail.com>
2024-09-11 15:20:32 +0300

    fixed: weed backgrund

--- js/weed.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
- const weed = document.querySelector('.weed-background.main-weed');
- if (header.classList.contains('fixed')) {
-  weed.classList.add('fixed');
- } else {
-  weed.classList.remove('fixed');
- }
-});
